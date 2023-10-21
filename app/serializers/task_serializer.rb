346d28b847529d45aca13309efa74cb4e33850cb
Wiktor Szymulewicz <wiktor@szymulewicz.com>
2023-10-21 11:58:56 +0200

    Cors, new serializer for task

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,7 +1,6 @@
-# frozen_string_literal: true
-
-# Serializer for the tasks
 class TaskSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description
+  attributes :id, :name, :description, :deadline
+  belongs_to :author, class_name: 'User'
   belongs_to :category
+  belongs_to :group
 end
