e7d8cab9544ccd02f9743492001270580ef5d3c6
Wikator <wiktor@szymulewicz.com>
2023-11-05 16:01:44 +0100

    Fixed migration

--- db/migrate/20231011163349_add_subject_and_group_to_tasks.rb
+++ db/migrate/20231011163349_add_subject_and_group_to_tasks.rb
@@ -3,10 +3,8 @@
 # Migration for adding subject and group to tasks
 class AddSubjectAndGroupToTasks < ActiveRecord::Migration[7.0]
   def change
-    change_table :tasks, bulk: true do |t|
-      t.add_column :subject_id, :integer
-      t.add_column :group_id, :integer
-    end
+    add_column :tasks, :subject_id, :integer
+    add_column :tasks, :group_id, :integer
 
     add_foreign_key :tasks, :subjects
     add_foreign_key :tasks, :groups
